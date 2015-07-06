351119e829ee412b06ad52b7736e2e437889fe34
thegoodhen <thegoodhen@gmail.com>
2015-07-06 15:02:49 +0200

    Changed the generator to match my specs

--- src/com/thegoodhen/launcher/Mane.java
+++ src/com/thegoodhen/launcher/Mane.java
@@ -20,12 +20,17 @@ public class Mane {
 		FullNameGenerator fng=new FullNameGenerator();
 		FirstSentenceDescGenerator fsdg=new FirstSentenceDescGenerator();
 		FullDescGenerator fdg=new FullDescGenerator();
-		IStringGenerator gens[]={fing, fng, fsdg, fdg};
+		IStringGenerator gens[]={fng, fsdg, fdg};
 		//PrintWriter writer = null;
 		String guiAppsPath="/home/thegoodhen/Documents/tgh_launcher_gui.txt";
 		String termAppsPath="/home/thegoodhen/Documents/tgh_launcher_term.txt";
 
 	try(PrintWriter guiWriter = new PrintWriter(guiAppsPath, "UTF-8"); PrintWriter termWriter= new PrintWriter(termAppsPath, "UTF-8");) {
+		
+		
+		//only one metainfo (name)
+		guiWriter.write("1");
+		termWriter.write("1");
 		
 		for(IStringGenerator isg:gens)
 		{
@@ -34,13 +39,17 @@ public class Mane {
 				File desktopFile=new File("/usr/share/applications/"+fing.generateString(f)+".desktop");
 				if(desktopFile.exists())//TODO: Actually check the content of the .desktop file if it exists
 				{
-					guiWriter.write("[["+fing.generateString(f)+"]]\n");
+					guiWriter.write(">>>BEGIN\n");
+					guiWriter.write(fing.generateString(f)+"\n");
 					guiWriter.write(isg.generateString(f)+"\n");
+					guiWriter.write(">>>END\n");
 				}
 				else
 				{
-					termWriter.write("[["+fing.generateString(f)+"]]\n");
+					termWriter.write(">>>BEGIN\n");
+					termWriter.write(fing.generateString(f)+"\n");
 					termWriter.write(isg.generateString(f)+"\n");
+					termWriter.write(">>>END\n");
 				}
 				//System.out.println("[["+fing.generateString(f)+"]]");
 				//System.out.println( isg.generateString(f));
