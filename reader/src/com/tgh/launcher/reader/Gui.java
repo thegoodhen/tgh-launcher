5c4f6baeef8e433475dfd69812287646160384af
unknown <peta.broz@seznam.cz>
2015-07-13 12:55:51 +0200

    wip to make buttons update

--- reader/src/com/tgh/launcher/reader/Gui.java
+++ reader/src/com/tgh/launcher/reader/Gui.java
@@ -8,6 +8,7 @@ import java.awt.Window.Type;
 import java.awt.FlowLayout;
 import java.awt.GraphicsDevice;
 import java.awt.Insets;
+import java.util.List;
 
 import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
@@ -16,6 +17,7 @@ import javax.swing.JButton;
 import java.awt.event.InputMethodListener;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Set;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputMethodEvent;
@@ -27,12 +29,14 @@ public class Gui {
 	static File guiFile;
 	ArrayList<App> results=new ArrayList<App>();
 	static AppList al;
+	static boolean packed;
 	
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		
+		packed = false;
 		guiFile=new File("/home/thegoodhen/Documents/tgh_launcher_gui.txt");
 		if (!guiFile.exists()){
 		guiFile=new File("C:/Users/Acer/tgh-launcher/reader/data/tgh_launcher_gui.txt");
@@ -40,6 +44,7 @@ public class Gui {
 		al=new AppList();
 		al.load(guiFile);
 		
+ 
 		
 		
 		EventQueue.invokeLater(new Runnable() {
@@ -106,28 +111,44 @@ public class Gui {
 		
 		String lineSep = System.getProperty("line.separator");
 		System.out.print(lineSep);
+		
+		
 		for(App a:results)
 		{
 			System.out.println(a.name+" "+a.relevance);
 			
-			JButton btnLaunch = new JButton(a.name); //add a.relevance to text ? seems irrelevant
-			btnLaunch.setMargin(new Insets(5,0,5,0));
 			
-			btnLaunch.addActionListener(new ActionListener(){
-					@Override
-					public void actionPerformed(ActionEvent e) {						
-						String appName = ((JButton)e.getSource()).getText();
-						System.out.println("The user, in their eternal wisdom, commands me to open " + appName);
-						//TODO: actually run the app
-						
-						}
-						
-					}
-			);
+			addBtn(a.name);
 			
-			frame.getContentPane().add(btnLaunch);
+			
+			if(!packed){	//the if needs to be removed when multiple lines of buttons become a thing.
 			frame.pack();
+			packed = true;
+				}
 		}
 		
 	}
+	
+	private void addBtn(String btnText){
+		
+		JButton btnLaunch = new JButton(btnText); 
+		btnLaunch.setMargin(new Insets(5,0,5,0));
+		
+		btnLaunch.addActionListener(new ActionListener(){
+				@Override
+				public void actionPerformed(ActionEvent e) {						
+					String appName = ((JButton)e.getSource()).getText(); //to be used for running the app
+					System.out.println("The user, in their eternal wisdom, commands me to open " + appName); //debug
+					//TODO: actually run the app
+					
+					}
+					
+				}
+		);
+		
+		frame.getContentPane().add(btnLaunch);
+		
+		
+	}
+	
 }
